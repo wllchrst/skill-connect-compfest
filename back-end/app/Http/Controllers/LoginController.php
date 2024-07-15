55afb74d9d5f9ef47d48df8ebc7d74e55060bfd2
Daniel Adamlu <adamludnl@gmail.com>
2024-07-15 18:17:08 +0700

    feat: added empty bvalidation to logincontroller

--- back-end/app/Http/Controllers/LoginController.php
+++ back-end/app/Http/Controllers/LoginController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function login(Request $request) {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if ($email === '' || $password === '') {
+            return response()->json([
+                'message' => 'Email and password are required',
+            ], 400);
+        }
+
         
     }
 }
--- back-end/app/Http/Controllers/RegisterController.php
+++ back-end/app/Http/Controllers/RegisterController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request) {
+        
+    }
 }
