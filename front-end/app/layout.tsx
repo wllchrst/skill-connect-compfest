c138dc7cc0dc659856e58cf6b8b1e9211369d060
wllchrst <willam.christian001@binus.ac.id>
2024-07-28 22:01:16 +0700

    ada rapih2 dikit doang frontend belum bisa ya styled component nya harus di config dikit

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -15,7 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
