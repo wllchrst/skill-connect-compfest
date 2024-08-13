d860520a1fb874423dd78b9315dc50b4df28a8ab
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-08-13 18:53:43 +0700

    feat: tambahin login page gambar doang dikti

--- front-end/app/pages/login/page.tsx
+++ front-end/app/pages/login/page.tsx
@@ -1,15 +1,18 @@
-'use client';
+"use client";
+import Image from "next/image";
+import loginPage from "../../assets/login-page.png";
 
 import FullScreenCenterLayout from "../../layout/full-screen-center-layout";
 
 export default function LoginPage() {
-    return <FullScreenCenterLayout>
-        <div className="flex p-2 gap-2 h-full w-full">
-            <div className="border border-pink w-2/3">
-            </div>
-            <div className="border border-pink w-1/3">
-            </div>
+  return (
+    <FullScreenCenterLayout>
+      <div className="flex p-2 gap-2 h-full w-full">
+        <div className="border border-pink w-2/3">
+          <Image src={loginPage} alt="" />
         </div>
-        
+        <div className="border border-pink w-1/3"></div>
+      </div>
     </FullScreenCenterLayout>
+  );
 }
