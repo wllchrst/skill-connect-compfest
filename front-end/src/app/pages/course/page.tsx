36b3085aab2d99c759f00341ba7e00d00ce64a3e
William Christian <william.christian001@binus.ac.id>
2024-09-04 14:02:44 +0700

    feat: ganti search jadi course page

--- front-end/src/app/pages/course/page.tsx
+++ front-end/src/app/pages/course/page.tsx
@@ -1,5 +1,11 @@
+import MainPageLayout from "@/app/layout/main-page-layout";
+
 function CoursePage() {
-  return <></>;
+  return (
+    <>
+      <MainPageLayout>course</MainPageLayout>
+    </>
+  );
 }
 
 export default CoursePage;
